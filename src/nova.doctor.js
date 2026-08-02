769b3dab7ded0fa9a33b30e8e0f035e662865068
MichaelPaulukonis <xraysmalevich@gmail.com>
2026-08-02 12:46:17 -0400

    refactor: Doctor uses steerToward, drops redundant re-randomize

--- src/nova.doctor.js
+++ src/nova.doctor.js
@@ -1,11 +1,8 @@
 import Mobile from './nova.mobile.js'
 
 export default class Doctor extends Mobile {
-  constructor (ctx, x, y, speed, noiseSpeed) {
-    super(ctx, x, y, speed, noiseSpeed)
-
-    this.x = this.ctx.random(this.ctx.width)
-    this.y = this.ctx.random(this.ctx.height)
+  constructor (ctx, x, y, speed, noiseSpeed, group) {
+    super(ctx, x, y, speed, noiseSpeed, group)
 
     this.sprite.image = '⛑'
     this.waiting = false
@@ -16,14 +13,9 @@ export default class Doctor extends Mobile {
 
     super.move()
 
-    const targetDistance = 100
-
-    // move towards a zombie
     for (let zombie of zombies.filter(z => !z.killed)) {
       if (this.proximityTo(zombie) < 100) {
-        let angle = this.ctx.atan2(this.y - zombie.y, this.x - zombie.x)
-        this.x -= this.ctx.cos(angle) * 5
-        this.y -= this.ctx.sin(angle) * 5
+        this.steerToward(zombie, 5)
         break
       }
     }
