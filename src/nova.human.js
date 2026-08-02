ff02f299d76206cca205aa950b970331a7f60e26
MichaelPaulukonis <xraysmalevich@gmail.com>
2026-08-02 12:38:50 -0400

    fix: remove double-movement bug in Human, dedupe steer/display via Mobile

--- src/nova.human.js
+++ src/nova.human.js
@@ -1,42 +1,23 @@
 import Mobile from './nova.mobile.js'
 
 export default class Human extends Mobile {
-  constructor (ctx, x, y, speed, noiseSpeed) {
-    super(ctx, x, y, speed, noiseSpeed)
-    
+  constructor (ctx, x, y, speed, noiseSpeed, group) {
+    super(ctx, x, y, speed, noiseSpeed, group)
+
     this.sprite.image = '😃'
   }
 
   move (zombies, player) {
     super.move()
-    
-    this.x += this.ctx.map(this.ctx.noise(this.noiseOffsetX), 0, 1, -2, 2)
-    this.y += this.ctx.map(this.ctx.noise(this.noiseOffsetY), 0, 1, -2, 2)
-    this.noiseOffsetX += 0.001
-    this.noiseOffsetY += 0.001
 
-    // Wrap-around logic
-    if (this.x < 0) this.x = this.ctx.width
-    if (this.x > this.ctx.width) this.x = 0
-    if (this.y < 0) this.y = this.ctx.height
-    if (this.y > this.ctx.height) this.y = 0
-
-    // Avoid player and zombies
     for (let zombie of zombies.filter(z => !z.killed)) {
-      if (this.ctx.dist(this.x, this.y, zombie.x, zombie.y) < 50) {
-        this.x += (this.x - zombie.x) * 0.05
-        this.y += (this.y - zombie.y) * 0.05
+      if (this.proximityTo(zombie) < 50) {
+        this.steerAway(zombie, 2)
         break
       }
     }
-    if (this.ctx.dist(this.x, this.y, player.x, player.y) < 50) {
-      this.x += (this.x - player.x) * 0.05
-      this.y += (this.y - player.y) * 0.05
+    if (this.proximityTo(player) < 50) {
+      this.steerAway(player, 2)
     }
-  }
-
-  display () {
-    this.sprite.x = this.x
-    this.sprite.y = this.y
   }
 }
